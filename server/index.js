7b19534ccd60e9962ae28b7230d207cb7043aeae
SamSarukhanyan <sarukhanyansam@gmail.com>
2026-02-02 01:07:14 +0400

    Update static file serving path to point to client distribution directory

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ app.use(cors({
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.static('../client/dist'));
 
 
 app.get('/api/get', (req, res) => {
